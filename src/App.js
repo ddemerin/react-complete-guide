c103170869759ef16ac23b31793634167d5a1fc8
Daniel Demerin <ddemerin.code@gmail.com>
2020-11-09 12:08:53 -0500

    Playing with State

--- src/App.js
+++ src/App.js
@@ -3,13 +3,26 @@ import "./App.css";
 import Person from "./Person/Person"
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Daniel', age: 35},
+      { name: 'Max', age: 28 },
+      { name: 'Dima', age: 25}
+    ]
+  }
+
+  switchNameHandler = () => {
+    console.log('Was clicked!');
+  }
+  
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <Person name="Daniel" age="35"/>
-        <Person name="Max" age="28"/>
-        <Person name="Dima" age="25">My Hobbies: Kiteboarding, Rock Climbing, Being Cute</Person>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].name}/>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].name}>My Hobbies: Kiteboarding, Rock Climbing, Being Cute</Person>
       </div>
     );
   }
